d7446d256b3dd78f24982d5efbb72824b805e64d
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: add API for disabling gestural navigation
    [jhonboy121]: adapted to A12L and A13
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -173,6 +173,7 @@ import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.keyguard.domain.interactor.AlternateBouncerInteractor;
 import com.android.systemui.keyguard.ui.binder.LightRevealScrimViewBinder;
 import com.android.systemui.keyguard.ui.viewmodel.LightRevealScrimViewModel;
+import com.android.systemui.model.SysUiState;
 import com.android.systemui.navigationbar.NavigationBarController;
 import com.android.systemui.navigationbar.NavigationBarView;
 import com.android.systemui.notetask.NoteTaskController;
@@ -764,6 +765,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         }
     };
 
+    private final SysUiState mSysUiState;
+
     /**
      * Public constructor for CentralSurfaces.
      *
@@ -871,7 +874,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
             UserTracker userTracker,
             Provider<FingerprintManager> fingerprintManager,
             TunerService tunerService,
-            ActivityStarter activityStarter
+            ActivityStarter activityStarter,
+            SysUiState sysUiState
     ) {
         mContext = context;
         mNotificationsController = notificationsController;
@@ -961,6 +965,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         mFingerprintManager = fingerprintManager;
         mTunerService = tunerService;
         mActivityStarter = activityStarter;
+        mSysUiState = sysUiState;
 
         mLockscreenShadeTransitionController = lockscreenShadeTransitionController;
         mStartingSurfaceOptional = startingSurfaceOptional;
@@ -3913,6 +3918,19 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
             KeyboardShortcutListSearch.dismiss();
         } else {
             KeyboardShortcuts.dismiss();
+        }
+    }
+
+    @Override
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        NotificationPanelViewController npvc =
+                mCentralSurfacesComponent.getNotificationPanelViewController();
+        if (npvc != null) {
+            npvc.setBlockedGesturalNavigation(blocked);
+            npvc.updateSystemUiStateFlags();
+        }
+        if (getNavigationBarView() != null) {
+            getNavigationBarView().setBlockedGesturalNavigation(blocked, mSysUiState);
         }
     }
 
