PixelExperience/frameworks_base

Squashed Import of PixelPropsUtils
commit 444afd520dd93038d8cd9f68438536240a2b608e (pixel/thirteen, m/thirteen)
Author: jhenrique09 <jhsv09@gmail.com>
Date:   Sun Jul 2 18:34:00 2023 -0300

    PixelPropsUtils: Remove pixel whitelist

    Change-Id: I28ff6643ebe656478d3f5dde28ca88cd3d7a7542

commit a8561c81d7e2cae1730a1e25b74823a980838b86
Author: jhenrique09 <jhsv09@gmail.com>
Date:   Thu Mar 23 16:22:25 2023 -0300

    PixelPropsUtils: Simplify GoogleCamera whitelisting

    Change-Id: I7db1b60343dff661a9f1c4116f689c3cc5afbdfc

    commit aa4c66949ad5552b3221cbd5b7fdf072dd7ce60e
    Author: jhenrique09 <jhsv09@gmail.com>
    Date:   Wed Mar 22 12:39:22 2023 -0300

        PixelPropsUtils: Spoof user build

        Change-Id: Ie33e0213a2608ecceea37b0e4cfb1115d6a0b788

    Change-Id: I7583251294028bd7c8a9372c4a9ee79b42213f94

commit 9c9e560381218d27dd3100789bdcb42634939521
Author: Sourajit Karmakar <sourajitk@statixos.com>
Date:   Tue Jun 20 21:45:18 2023 -0400

    PixelPropsUtils: Enable new Velvet Weather UI

    Change-Id: I45310071f0267be1ea26f7c00044a56a763a3e6a

    * Adapt to current PixelPropsUtils

    Original commit:
    https://github.com/StatiXOS/android_frameworks_base/commit/ce72977956b2e787230e429ef98a6ac537454347

    Change-Id: I0989ef14aec6fabcc0733232ddb65fd96fe7d80d
    Signed-off-by: someone5678 <nemui3353@gmail.com>

commit ad3fee4764092a5f8b2b36c9efda0cb55e757901
Author: aswin7469 <aswinas@pixysos.com>
Date:   Tue Jun 20 12:33:17 2023 +0530

    PixelPropUtils: Misc updates

    * unspoof gms pkg as whole, only spoof required processes
    * cleanup some duplicates
    * spoof play store to Pixel 5

    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Change-Id: Ifb9a4ed5a864e293c52f7d96a2f4d27cd6152c96

commit 232dbb7b0974819b2386cf59f3f3cf9d1c5e3ee0
Author: aswin7469 <aswinas@pixysos.com>
Date:   Sat Jun 17 13:53:45 2023 +0530

    PixelPropUtils: Add emojiwalls and cinematiceffects p

core/java/android/app/Instrumentation.java
@@ -64,6 +64,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1242,6 +1244,8 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1259,6 +1263,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
 

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -77,6 +77,8 @@ import java.util.Set;
 
 import javax.crypto.SecretKey;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
  * it can be created via the {@link java.security.KeyStore#getInstance(String)
@@ -164,6 +166,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
+
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -268,6 +268,7 @@ import com.android.server.sdksandbox.SdkSandboxManagerLocal;
 import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
+import com.android.internal.util.custom.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1961,7 +1962,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -2991,7 +2994,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 

bhaskar966/frameworks_base-derp

core/java/android/app/Instrumentation.java
@@ -67,6 +67,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.derp.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1281,6 +1283,8 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1298,6 +1302,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        String packageName = context.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
 

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -87,6 +87,8 @@ import java.util.NoSuchElementException;
 
 import javax.crypto.SecretKey;
 
+import com.android.internal.util.derp.PixelPropsUtils;
+
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
  * it can be created via the {@link java.security.KeyStore#getInstance(String)
@@ -174,6 +176,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
+
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -278,6 +278,7 @@ import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
+import com.android.internal.util.derp.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1954,7 +1955,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3064,7 +3067,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 