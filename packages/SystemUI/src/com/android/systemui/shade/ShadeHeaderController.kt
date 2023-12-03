LeafOS-Project/android_frameworks_base

SystemUI: Add dual-tone light and dark themes for QS
Author: Danny Lin <danny@kdrag0n.dev>
Date:   Wed Oct 6 18:40:30 2021 -0700

    SystemUI: Make QS panel change on UI mode changes

    This is a prerequisite for making the QS panel follow the light system
    theme setting.

    Change-Id: Iac4c96ccb3845812ca3df820bf27dc533816f72e
    Signed-off-by: althafvly <althafvly@gmail.com>

Author: Danny Lin <danny@kdrag0n.dev>
Date:   Wed Oct 6 18:41:11 2021 -0700

    Revert "Do not re-inflate QS and SB when CONFIG_UI_MODE"

    This is a prerequisite for making the QS panel follow the light system
    mode setting.

    Change-Id: Ibad399ece587505559cc73febdda2f2d8558e94d

Author: Danny Lin <danny@kdrag0n.dev>
Date:   Mon Oct 11 19:24:58 2021 -0700

    SystemUI: Follow light/dark theme in quick settings

    Android 12's dual-tone style where the quick settings panel is always
    dark makes the light theme look like a second-class citizen. Pure black
    also looks out-of-place next to QS tiles and the notification panel
    because dynamic themes don't affect it at all.

    Revert to the ~Beta 1 style where quick settings used the same theme as
    the notification shade.

    - colorAccentPrimary has been replaced with colorAccent for contrast in
      light mode, because colorAccentPrimary is system_accent1_100 (dark
      accent color)
    - Footer chips have been converted to surfaces (similar to QS tiles and
      notifications), which makes more sense with the new style
    - The QS background is now the same as the notification shade background
      in both light and dark modes

    Demo screenshots (with dual-tone commit):
    https://twitter.com/kdrag0n/status/1445922541218922496

    [kdrag0n: ported to 12L]

    Change-Id: I3d45b72f0f119e100505409d178ab8f698993881

Author: Danny Lin <danny@kdrag0n.dev>
Date:   Mon Oct 11 19:25:02 2021 -0700

    SystemUI: Initialize QS tiles in inactive state

    Now that the QS fragment is recreated when changing the

packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -22,7 +22,10 @@ import android.annotation.IdRes
 import android.app.PendingIntent
 import android.app.StatusBarManager
 import android.content.Intent
+import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
@@ -90,6 +93,7 @@ constructor(
     private val privacyIconsController: HeaderPrivacyIconsController,
     private val insetsProvider: StatusBarContentInsetsProvider,
     private val configurationController: ConfigurationController,
+    private val context: Context,
     private val variableDateViewControllerFactory: VariableDateViewController.Factory,
     @Named(SHADE_HEADER) private val batteryMeterViewController: BatteryMeterViewController,
     private val dumpManager: DumpManager,
@@ -136,6 +140,7 @@ constructor(
     private var cutout: DisplayCutout? = null
     private var lastInsets: WindowInsets? = null
     private var nextAlarmIntent: PendingIntent? = null
+    private var textColorPrimary = Color.TRANSPARENT
 
     private var qsDisabled = false
     private var visible = false
@@ -263,6 +268,10 @@ constructor(
                 updateResources()
                 updateCarrierGroupPadding()
             }
+
+            override fun onUiModeChanged() {
+                updateResources()
+            }
         }
 
     private val nextAlarmCallback =
@@ -310,6 +319,7 @@ constructor(
         demoModeController.addCallback(demoModeReceiver)
         statusBarIconController.addIconGroup(iconManager)
         nextAlarmController.addCallback(nextAlarmCallback)
+        updateResources()
     }
 
     override fun onViewDetached() {
@@ -502,6 +512,23 @@ constructor(
         header.setPadding(padding, header.paddingTop, padding, header.paddingBottom)
         updateQQSPaddings()
         qsBatteryModeController.updateResources()
+
+        val fillColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        iconManager.setTint(fillColor)
+        val textColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        val colorStateList = Utils.getColorAttr(context, android.R.attr.textColorPrimary)
+        if (textColor != textColorPrimary) {
+            val textColorSecondary = Utils.getColorAttrDefaultColor(context,
+                    android.R.attr.textColorSecondary)
+            textColorPrimary = textColor
+            if (iconManager != null) {
+                iconManager.setTint(textColor)
+            }
+            clock.setTextColor(textColorPrimary)
+            date.setTextColor(textColorPrimary)
+            qsCarrierGroup.updateColors(textColorPrimary, colorStateList)
+            batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
+        }
     }
 
     private fun updateQQSPaddings() {

bhaskar966/frameworks_base-derp

packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -22,7 +22,10 @@ import android.annotation.IdRes
 import android.app.PendingIntent
 import android.app.StatusBarManager
 import android.content.Intent
+import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
@@ -90,6 +93,7 @@ constructor(
     private val privacyIconsController: HeaderPrivacyIconsController,
     private val insetsProvider: StatusBarContentInsetsProvider,
     private val configurationController: ConfigurationController,
+    private val context: Context,
     private val variableDateViewControllerFactory: VariableDateViewController.Factory,
     @Named(SHADE_HEADER) private val batteryMeterViewController: BatteryMeterViewController,
     private val dumpManager: DumpManager,
@@ -136,6 +140,7 @@ constructor(
     private var cutout: DisplayCutout? = null
     private var lastInsets: WindowInsets? = null
     private var nextAlarmIntent: PendingIntent? = null
+    private var textColorPrimary = Color.TRANSPARENT
 
     private var qsDisabled = false
     private var visible = false
@@ -263,6 +268,10 @@ constructor(
                 updateResources()
                 updateCarrierGroupPadding()
             }
+
+            override fun onUiModeChanged() {
+                updateResources()
+            }
         }
 
     private val nextAlarmCallback =
@@ -310,6 +319,7 @@ constructor(
         demoModeController.addCallback(demoModeReceiver)
         statusBarIconController.addIconGroup(iconManager)
         nextAlarmController.addCallback(nextAlarmCallback)
+        updateResources()
     }
 
     override fun onViewDetached() {
@@ -502,6 +512,23 @@ constructor(
         header.setPadding(padding, header.paddingTop, padding, header.paddingBottom)
         updateQQSPaddings()
         qsBatteryModeController.updateResources()
+
+        val fillColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        iconManager.setTint(fillColor)
+        val textColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        val colorStateList = Utils.getColorAttr(context, android.R.attr.textColorPrimary)
+        if (textColor != textColorPrimary) {
+            val textColorSecondary = Utils.getColorAttrDefaultColor(context,
+                    android.R.attr.textColorSecondary)
+            textColorPrimary = textColor
+            if (iconManager != null) {
+                iconManager.setTint(textColor)
+            }
+            clock.setTextColor(textColorPrimary)
+            date.setTextColor(textColorPrimary)
+            mShadeCarrierGroup.updateColors(textColorPrimary, colorStateList)
+            batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
+        }
     }
 
     private fun updateQQSPaddings() {