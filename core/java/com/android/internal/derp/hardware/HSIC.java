LMODroid/platform_frameworks_base

base: Introduce LiveDisplay from Lineage
Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d

core/java/com/android/internal/libremobileos/hardware/HSIC.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.libremobileos.hardware;
+
+import android.graphics.Color;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import java.util.Locale;
+
+public class HSIC implements Parcelable {
+
+    private final float mHue;
+    private final float mSaturation;
+    private final float mIntensity;
+    private final float mContrast;
+    private final float mSaturationThreshold;
+
+    public HSIC(float hue, float saturation, float intensity,
+                float contrast, float saturationThreshold) {
+        mHue = hue;
+        mSaturation = saturation;
+        mIntensity = intensity;
+        mContrast = contrast;
+        mSaturationThreshold = saturationThreshold;
+    }
+
+    public float getHue() {
+        return mHue;
+    }
+
+    public float getSaturation() {
+        return mSaturation;
+    }
+
+    public float getIntensity() {
+        return mIntensity;
+    }
+
+    public float getContrast() {
+        return mContrast;
+    }
+
+    public float getSaturationThreshold() {
+        return mSaturationThreshold;
+    }
+
+    public String flatten() {
+        return String.format(Locale.US, "%f|%f|%f|%f|%f", mHue, mSaturation,
+                mIntensity, mContrast, mSaturationThreshold);
+    }
+
+    public static HSIC unflattenFrom(String flat) throws NumberFormatException {
+        final String[] unflat = TextUtils.split(flat, "\\|");
+        if (unflat.length != 4 && unflat.length != 5) {
+            throw new NumberFormatException("Failed to unflatten HSIC values: " + flat);
+        }
+        return new HSIC(Float.parseFloat(unflat[0]), Float.parseFloat(unflat[1]),
+                Float.parseFloat(unflat[2]), Float.parseFloat(unflat[3]),
+                unflat.length == 5 ? Float.parseFloat(unflat[4]) : 0.0f);
+    }
+
+    public int[] toRGB() {
+        final int c = Color.HSVToColor(toFloatArray());
+        return new int[] { Color.red(c), Color.green(c), Color.blue(c) };
+    }
+
+    public float[] toFloatArray() {
+        return new float[] { mHue, mSaturation, mIntensity, mContrast, mSaturationThreshold };
+    }
+
+    public static HSIC fromFloatArray(float[] hsic) {
+        if (hsic.length == 5) {
+            return new HSIC(hsic[0], hsic[1], hsic[2], hsic[3], hsic[4]);
+        } else if (hsic.length == 4) {
+            return new HSIC(hsic[0], hsic[1], hsic[2], hsic[3], 0.0f);
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "HSIC={ hue=%f saturation=%f intensity=%f " +
+                             "contrast=%f saturationThreshold=%f }",
+                mHue, mSaturation, mIntensity, mContrast, mSaturationThreshold);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeFloatArray(toFloatArray());
+    }
+
+    /**
+     * @hide
+     */
+    public static final Parcelable.Creator<HSIC> CREATOR =
+            new Parcelable.Creator<HSIC>() {
+                public HSIC createFromParcel(Parcel in) {
+                    float[] fromParcel = new float[5];
+                    in.readFloatArray(fromParcel);
+                    return HSIC.fromFloatArray(fromParcel);
+                }
+
+                @Override
+                public HSIC[] newArray(int size) {
+                    return new HSIC[size];
+                }
+            };
+};

services/core/java/com/android/server/libremobileos/display/LiveDisplayFeature.java
@@ -1,0 +1,184 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.libremobileos.display;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.server.libremobileos.common.UserContentObserver;
+import com.android.server.libremobileos.display.LiveDisplayService.State;
+import com.android.server.libremobileos.display.TwilightTracker.TwilightState;
+
+import java.io.PrintWriter;
+import java.util.BitSet;
+
+import static com.android.server.libremobileos.display.LiveDisplayService.ALL_CHANGED;
+import static com.android.server.libremobileos.display.LiveDisplayService.DISPLAY_CHANGED;
+import static com.android.server.libremobileos.display.LiveDisplayService.MODE_CHANGED;
+import static com.android.server.libremobileos.display.LiveDisplayService.TWILIGHT_CHANGED;
+
+public abstract class LiveDisplayFeature {
+
+    protected static final String TAG = "LiveDisplay";
+    protected static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    protected final Context mContext;
+    protected final Handler mHandler;
+    protected final boolean mNightDisplayAvailable;
+
+    private SettingsObserver mSettingsObserver;
+    private State mState;
+
+    public LiveDisplayFeature(Context context, Handler handler) {
+        mContext = context;
+        mHandler = handler;
+        mNightDisplayAvailable = ColorDisplayManager.isNightDisplayAvailable(mContext);
+    }
+
+    public abstract void onStart();
+
+    protected abstract void onSettingsChanged(Uri uri);
+
+    public abstract void dump(PrintWriter pw);
+
+    public abstract boolean getCapabilities(final BitSet caps);
+
+    protected abstract void onUpdate();
+
+    void update(final int flags, final State state) {
+        mState = state;
+        if ((flags & DISPLAY_CHANGED) != 0) {
+            onScreenStateChanged();
+        }
+        if (((flags & TWILIGHT_CHANGED) != 0) && mState.mTwilight != null) {
+            onTwilightUpdated();
+        }
+        if ((flags & MODE_CHANGED) != 0) {
+            onUpdate();
+        }
+        if (flags == ALL_CHANGED) {
+            onSettingsChanged(null);
+        }
+    }
+
+    void start() {
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(mHandler);
+            onStart();
+        }
+    }
+
+    public void onDestroy() {
+        mSettingsObserver.unregister();
+    }
+
+    protected void onScreenStateChanged() { }
+
+    protected void onTwilightUpdated() { }
+
+    protected final void registerSettings(Uri... settings) {
+        mSettingsObserver.register(settings);
+    }
+
+    protected final boolean getBoolean(String setting, boolean defaultValue) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                setting, (defaultValue ? 1 : 0), UserHandle.USER_CURRENT) == 1;
+    }
+
+    protected final void putBoolean(String setting, boolean value) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                setting, (value ? 1 : 0), UserHandle.USER_CURRENT);
+    }
+
+    protected final int getInt(String setting, int defaultValue) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                setting, defaultValue, UserHandle.USER_CURRENT);
+    }
+
+    protected final void putInt(String setting, int value) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                setting, value, UserHandle.USER_CURRENT);
+    }
+
+    protected final String getString(String setting) {
+        return Settings.System.getStringForUser(mContext.getContentResolver(),
+                setting, UserHandle.USER_CURRENT);
+    }
+
+    protected final void putString(String setting, String value) {
+        Settings.System.putStringForUser(mContext.getContentResolver(),
+                setting, value, UserHandle.USER_CURRENT);
+    }
+
+    protected final boolean isLowPowerMode() {
+        return mState.mLowPowerMode;
+    }
+
+    protected final int getMode() {
+        return mState.mMode;
+    }
+
+    protected final boolean isScreenOn() {
+        return mState.mScreenOn;
+    }
+
+    protected final TwilightState getTwilight() {
+        return mState.mTwilight;
+    }
+
+    public final boolean isNight() {
+        return mState.mTwilight != null && mState.mTwilight.isNight();
+    }
+
+    final class SettingsObserver extends UserContentObserver {
+
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void register(Uri... uris) {
+            final ContentResolver cr = mContext.getContentResolver();
+            for (Uri uri : uris) {
+                cr.registerContentObserver(uri, false, this, UserHandle.USER_ALL);
+            }
+
+            observe();
+        }
+
+        public void unregister() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+            unobserve();
+        }
+
+        @Override
+        protected void update() {
+            onSettingsChanged(null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            onSettingsChanged(uri);
+        }
+    }
+
+}

services/core/java/com/android/server/libremobileos/display/TwilightCalculator.java
@@ -1,0 +1,123 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.libremobileos.display;
+
+import android.text.format.DateUtils;
+import android.util.FloatMath;
+
+/** @hide */
+public class TwilightCalculator {
+
+    /** Value of {@link #mState} if it is currently day */
+    public static final int DAY = 0;
+
+    /** Value of {@link #mState} if it is currently night */
+    public static final int NIGHT = 1;
+
+    private static final float DEGREES_TO_RADIANS = (float) (Math.PI / 180.0f);
+
+    // element for calculating solar transit.
+    private static final float J0 = 0.0009f;
+
+    // correction for civil twilight
+    private static final float ALTIDUTE_CORRECTION_CIVIL_TWILIGHT = -0.104719755f;
+
+    // coefficients for calculating Equation of Center.
+    private static final float C1 = 0.0334196f;
+    private static final float C2 = 0.000349066f;
+    private static final float C3 = 0.000005236f;
+
+    private static final float OBLIQUITY = 0.40927971f;
+
+    // Java time on Jan 1, 2000 12:00 UTC.
+    private static final long UTC_2000 = 946728000000L;
+
+    /**
+     * Time of sunset (civil twilight) in milliseconds or -1 in the case the day
+     * or night never ends.
+     */
+    public long mSunset;
+
+    /**
+     * Time of sunrise (civil twilight) in milliseconds or -1 in the case the
+     * day or night never ends.
+     */
+    public long mSunrise;
+
+    /** Current state */
+    public int mState;
+
+    /**
+     * calculates the civil twilight bases on time and geo-coordinates.
+     *
+     * @param time time in milliseconds.
+     * @param latiude latitude in degrees.
+     * @param longitude latitude in degrees.
+     */
+    public void calculateTwilight(long time, double latiude, double longitude) {
+        final float daysSince2000 = (float) (time - UTC_2000) / DateUtils.DAY_IN_MILLIS;
+
+        // mean anomaly
+        final float meanAnomaly = 6.240059968f + daysSince2000 * 0.01720197f;
+
+        // true anomaly
+        final float trueAnomaly = meanAnomaly + C1 * FloatMath.sin(meanAnomaly) + C2
+                * FloatMath.sin(2 * meanAnomaly) + C3 * FloatMath.sin(3 * meanAnomaly);
+
+        // ecliptic longitude
+        final float solarLng = trueAnomaly + 1.796593063f + (float) Math.PI;
+
+        // solar transit in days since 2000
+        final double arcLongitude = -longitude / 360;
+        float n = Math.round(daysSince2000 - J0 - arcLongitude);
+        double solarTransitJ2000 = n + J0 + arcLongitude + 0.0053f * FloatMath.sin(meanAnomaly)
+                + -0.0069f * FloatMath.sin(2 * solarLng);
+
+        // declination of sun
+        double solarDec = Math.asin(FloatMath.sin(solarLng) * FloatMath.sin(OBLIQUITY));
+
+        final double latRad = latiude * DEGREES_TO_RADIANS;
+
+        double cosHourAngle = (FloatMath.sin(ALTIDUTE_CORRECTION_CIVIL_TWILIGHT) - Math.sin(latRad)
+                * Math.sin(solarDec)) / (Math.cos(latRad) * Math.cos(solarDec));
+        // The day or night never ends for the given date and location, if this value is out of
+        // range.
+        if (cosHourAngle >= 1) {
+            mState = NIGHT;
+            mSunset = -1;
+            mSunrise = -1;
+            return;
+        } else if (cosHourAngle <= -1) {
+            mState = DAY;
+            mSunset = -1;
+            mSunrise = -1;
+            return;
+        }
+
+        float hourAngle = (float) (Math.acos(cosHourAngle) / (2 * Math.PI));
+
+        mSunset = Math.round((solarTransitJ2000 + hourAngle) * DateUtils.DAY_IN_MILLIS) + UTC_2000;
+        mSunrise = Math.round((solarTransitJ2000 - hourAngle) * DateUtils.DAY_IN_MILLIS) + UTC_2000;
+
+        if (mSunrise < time && mSunset > time) {
+            mState = DAY;
+        } else {
+            mState = NIGHT;
+        }
+    }
+
+}

bhaskar966/frameworks_base-derp

core/java/com/android/internal/derp/hardware/HSIC.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.derp.hardware;
+
+import android.graphics.Color;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import java.util.Locale;
+
+public class HSIC implements Parcelable {
+
+    private final float mHue;
+    private final float mSaturation;
+    private final float mIntensity;
+    private final float mContrast;
+    private final float mSaturationThreshold;
+
+    public HSIC(float hue, float saturation, float intensity,
+                float contrast, float saturationThreshold) {
+        mHue = hue;
+        mSaturation = saturation;
+        mIntensity = intensity;
+        mContrast = contrast;
+        mSaturationThreshold = saturationThreshold;
+    }
+
+    public float getHue() {
+        return mHue;
+    }
+
+    public float getSaturation() {
+        return mSaturation;
+    }
+
+    public float getIntensity() {
+        return mIntensity;
+    }
+
+    public float getContrast() {
+        return mContrast;
+    }
+
+    public float getSaturationThreshold() {
+        return mSaturationThreshold;
+    }
+
+    public String flatten() {
+        return String.format(Locale.US, "%f|%f|%f|%f|%f", mHue, mSaturation,
+                mIntensity, mContrast, mSaturationThreshold);
+    }
+
+    public static HSIC unflattenFrom(String flat) throws NumberFormatException {
+        final String[] unflat = TextUtils.split(flat, "\\|");
+        if (unflat.length != 4 && unflat.length != 5) {
+            throw new NumberFormatException("Failed to unflatten HSIC values: " + flat);
+        }
+        return new HSIC(Float.parseFloat(unflat[0]), Float.parseFloat(unflat[1]),
+                Float.parseFloat(unflat[2]), Float.parseFloat(unflat[3]),
+                unflat.length == 5 ? Float.parseFloat(unflat[4]) : 0.0f);
+    }
+
+    public int[] toRGB() {
+        final int c = Color.HSVToColor(toFloatArray());
+        return new int[] { Color.red(c), Color.green(c), Color.blue(c) };
+    }
+
+    public float[] toFloatArray() {
+        return new float[] { mHue, mSaturation, mIntensity, mContrast, mSaturationThreshold };
+    }
+
+    public static HSIC fromFloatArray(float[] hsic) {
+        if (hsic.length == 5) {
+            return new HSIC(hsic[0], hsic[1], hsic[2], hsic[3], hsic[4]);
+        } else if (hsic.length == 4) {
+            return new HSIC(hsic[0], hsic[1], hsic[2], hsic[3], 0.0f);
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "HSIC={ hue=%f saturation=%f intensity=%f " +
+                             "contrast=%f saturationThreshold=%f }",
+                mHue, mSaturation, mIntensity, mContrast, mSaturationThreshold);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeFloatArray(toFloatArray());
+    }
+
+    /**
+     * @hide
+     */
+    public static final Parcelable.Creator<HSIC> CREATOR =
+            new Parcelable.Creator<HSIC>() {
+                public HSIC createFromParcel(Parcel in) {
+                    float[] fromParcel = new float[5];
+                    in.readFloatArray(fromParcel);
+                    return HSIC.fromFloatArray(fromParcel);
+                }
+
+                @Override
+                public HSIC[] newArray(int size) {
+                    return new HSIC[size];
+                }
+            };
+};

services/core/java/com/android/server/derp/display/LiveDisplayFeature.java
@@ -1,0 +1,184 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.derp.display;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.hardware.display.ColorDisplayManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.server.derp.common.UserContentObserver;
+import com.android.server.derp.display.LiveDisplayService.State;
+import com.android.server.derp.display.TwilightTracker.TwilightState;
+
+import java.io.PrintWriter;
+import java.util.BitSet;
+
+import static com.android.server.derp.display.LiveDisplayService.ALL_CHANGED;
+import static com.android.server.derp.display.LiveDisplayService.DISPLAY_CHANGED;
+import static com.android.server.derp.display.LiveDisplayService.MODE_CHANGED;
+import static com.android.server.derp.display.LiveDisplayService.TWILIGHT_CHANGED;
+
+public abstract class LiveDisplayFeature {
+
+    protected static final String TAG = "LiveDisplay";
+    protected static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    protected final Context mContext;
+    protected final Handler mHandler;
+    protected final boolean mNightDisplayAvailable;
+
+    private SettingsObserver mSettingsObserver;
+    private State mState;
+
+    public LiveDisplayFeature(Context context, Handler handler) {
+        mContext = context;
+        mHandler = handler;
+        mNightDisplayAvailable = ColorDisplayManager.isNightDisplayAvailable(mContext);
+    }
+
+    public abstract void onStart();
+
+    protected abstract void onSettingsChanged(Uri uri);
+
+    public abstract void dump(PrintWriter pw);
+
+    public abstract boolean getCapabilities(final BitSet caps);
+
+    protected abstract void onUpdate();
+
+    void update(final int flags, final State state) {
+        mState = state;
+        if ((flags & DISPLAY_CHANGED) != 0) {
+            onScreenStateChanged();
+        }
+        if (((flags & TWILIGHT_CHANGED) != 0) && mState.mTwilight != null) {
+            onTwilightUpdated();
+        }
+        if ((flags & MODE_CHANGED) != 0) {
+            onUpdate();
+        }
+        if (flags == ALL_CHANGED) {
+            onSettingsChanged(null);
+        }
+    }
+
+    void start() {
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(mHandler);
+            onStart();
+        }
+    }
+
+    public void onDestroy() {
+        mSettingsObserver.unregister();
+    }
+
+    protected void onScreenStateChanged() { }
+
+    protected void onTwilightUpdated() { }
+
+    protected final void registerSettings(Uri... settings) {
+        mSettingsObserver.register(settings);
+    }
+
+    protected final boolean getBoolean(String setting, boolean defaultValue) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                setting, (defaultValue ? 1 : 0), UserHandle.USER_CURRENT) == 1;
+    }
+
+    protected final void putBoolean(String setting, boolean value) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                setting, (value ? 1 : 0), UserHandle.USER_CURRENT);
+    }
+
+    protected final int getInt(String setting, int defaultValue) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                setting, defaultValue, UserHandle.USER_CURRENT);
+    }
+
+    protected final void putInt(String setting, int value) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                setting, value, UserHandle.USER_CURRENT);
+    }
+
+    protected final String getString(String setting) {
+        return Settings.System.getStringForUser(mContext.getContentResolver(),
+                setting, UserHandle.USER_CURRENT);
+    }
+
+    protected final void putString(String setting, String value) {
+        Settings.System.putStringForUser(mContext.getContentResolver(),
+                setting, value, UserHandle.USER_CURRENT);
+    }
+
+    protected final boolean isLowPowerMode() {
+        return mState.mLowPowerMode;
+    }
+
+    protected final int getMode() {
+        return mState.mMode;
+    }
+
+    protected final boolean isScreenOn() {
+        return mState.mScreenOn;
+    }
+
+    protected final TwilightState getTwilight() {
+        return mState.mTwilight;
+    }
+
+    public final boolean isNight() {
+        return mState.mTwilight != null && mState.mTwilight.isNight();
+    }
+
+    final class SettingsObserver extends UserContentObserver {
+
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void register(Uri... uris) {
+            final ContentResolver cr = mContext.getContentResolver();
+            for (Uri uri : uris) {
+                cr.registerContentObserver(uri, false, this, UserHandle.USER_ALL);
+            }
+
+            observe();
+        }
+
+        public void unregister() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+            unobserve();
+        }
+
+        @Override
+        protected void update() {
+            onSettingsChanged(null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            onSettingsChanged(uri);
+        }
+    }
+
+}

services/core/java/com/android/server/derp/display/TwilightCalculator.java
@@ -1,0 +1,123 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.derp.display;
+
+import android.text.format.DateUtils;
+import android.util.FloatMath;
+
+/** @hide */
+public class TwilightCalculator {
+
+    /** Value of {@link #mState} if it is currently day */
+    public static final int DAY = 0;
+
+    /** Value of {@link #mState} if it is currently night */
+    public static final int NIGHT = 1;
+
+    private static final float DEGREES_TO_RADIANS = (float) (Math.PI / 180.0f);
+
+    // element for calculating solar transit.
+    private static final float J0 = 0.0009f;
+
+    // correction for civil twilight
+    private static final float ALTIDUTE_CORRECTION_CIVIL_TWILIGHT = -0.104719755f;
+
+    // coefficients for calculating Equation of Center.
+    private static final float C1 = 0.0334196f;
+    private static final float C2 = 0.000349066f;
+    private static final float C3 = 0.000005236f;
+
+    private static final float OBLIQUITY = 0.40927971f;
+
+    // Java time on Jan 1, 2000 12:00 UTC.
+    private static final long UTC_2000 = 946728000000L;
+
+    /**
+     * Time of sunset (civil twilight) in milliseconds or -1 in the case the day
+     * or night never ends.
+     */
+    public long mSunset;
+
+    /**
+     * Time of sunrise (civil twilight) in milliseconds or -1 in the case the
+     * day or night never ends.
+     */
+    public long mSunrise;
+
+    /** Current state */
+    public int mState;
+
+    /**
+     * calculates the civil twilight bases on time and geo-coordinates.
+     *
+     * @param time time in milliseconds.
+     * @param latiude latitude in degrees.
+     * @param longitude latitude in degrees.
+     */
+    public void calculateTwilight(long time, double latiude, double longitude) {
+        final float daysSince2000 = (float) (time - UTC_2000) / DateUtils.DAY_IN_MILLIS;
+
+        // mean anomaly
+        final float meanAnomaly = 6.240059968f + daysSince2000 * 0.01720197f;
+
+        // true anomaly
+        final float trueAnomaly = meanAnomaly + C1 * FloatMath.sin(meanAnomaly) + C2
+                * FloatMath.sin(2 * meanAnomaly) + C3 * FloatMath.sin(3 * meanAnomaly);
+
+        // ecliptic longitude
+        final float solarLng = trueAnomaly + 1.796593063f + (float) Math.PI;
+
+        // solar transit in days since 2000
+        final double arcLongitude = -longitude / 360;
+        float n = Math.round(daysSince2000 - J0 - arcLongitude);
+        double solarTransitJ2000 = n + J0 + arcLongitude + 0.0053f * FloatMath.sin(meanAnomaly)
+                + -0.0069f * FloatMath.sin(2 * solarLng);
+
+        // declination of sun
+        double solarDec = Math.asin(FloatMath.sin(solarLng) * FloatMath.sin(OBLIQUITY));
+
+        final double latRad = latiude * DEGREES_TO_RADIANS;
+
+        double cosHourAngle = (FloatMath.sin(ALTIDUTE_CORRECTION_CIVIL_TWILIGHT) - Math.sin(latRad)
+                * Math.sin(solarDec)) / (Math.cos(latRad) * Math.cos(solarDec));
+        // The day or night never ends for the given date and location, if this value is out of
+        // range.
+        if (cosHourAngle >= 1) {
+            mState = NIGHT;
+            mSunset = -1;
+            mSunrise = -1;
+            return;
+        } else if (cosHourAngle <= -1) {
+            mState = DAY;
+            mSunset = -1;
+            mSunrise = -1;
+            return;
+        }
+
+        float hourAngle = (float) (Math.acos(cosHourAngle) / (2 * Math.PI));
+
+        mSunset = Math.round((solarTransitJ2000 + hourAngle) * DateUtils.DAY_IN_MILLIS) + UTC_2000;
+        mSunrise = Math.round((solarTransitJ2000 - hourAngle) * DateUtils.DAY_IN_MILLIS) + UTC_2000;
+
+        if (mSunrise < time && mSunset > time) {
+            mState = DAY;
+        } else {
+            mState = NIGHT;
+        }
+    }
+
+}